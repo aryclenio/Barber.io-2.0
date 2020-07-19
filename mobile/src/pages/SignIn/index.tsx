88a587ec47b7ac142c3cb1045b9b8d21ca37891e
aryclenio <aryclenio2010@gmail.com>
2020-07-19 20:10:30 -0300

    :rocket: Creating Button and Input components

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Image } from 'react-native'
+
+import Input from '../../components/Input';
+import Button from '../../components/Button'
+
 import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
-    return (<Container>
-        <Image source={logoImg} />
-        <Title>Faça seu Logon</Title>
-    </Container>);
+    return (
+        <Container>
+            <Image source={logoImg} />
+            <Title>Faça seu Logon</Title>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button>Entrar</Button>
+        </Container>
+    );
 }
 
 export default SignIn;
