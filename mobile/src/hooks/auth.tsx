5e66a322993ce2809f7217befb64ba9c8b71b30d
aryclenio <aryclenio2010@gmail.com>
2020-07-21 15:06:31 -0300

    :rocket: Implementing auth

---
+++ mobile/src/hooks/auth.tsx
@@ -0,0 +1,76 @@
+import React, { createContext, useCallback, useState, useContext, useEffect } from 'react';
+import api from '../services/api';
+import AsyncStorage from '@react-native-community/async-storage'
+
+interface AuthState {
+    token: string;
+    user: Record<string, unknown>;
+}
+
+interface SignInCredentials {
+    email: string;
+    password: string;
+}
+
+interface AuthContextData {
+    user: Record<string, unknown>;
+    signIn(credentials: SignInCredentials): Promise<void>;
+    signOut(): void;
+}
+
+// Inicializando contexto como objeto vazio
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+    const [data, setData] = useState<AuthState>({} as AuthState);
+
+    useEffect(() => {
+        async function loadStoragedData(): Promise<void> {
+            const [token, user] = await AsyncStorage.multiGet([
+                '@Barbeiro:token',
+                '@Barbeiro:user'
+            ]);
+
+            if (token[1] && user[1]) {
+                setData({ token: token[1], user: JSON.parse(user[1]) });
+            }
+
+        }
+        loadStoragedData();
+    }, [])
+
+    const signIn = useCallback(async ({ email, password }) => {
+        const response = await api.post('sessions', {
+            email,
+            password,
+        });
+
+        const { token, user } = response.data;
+        await AsyncStorage.multiSet([
+            ['@Barbeiro:token', token],
+            ['@Barbeiro:user', JSON.stringify(user)]
+        ])
+        setData({ token, user });
+    }, []);
+
+    const signOut = useCallback(async () => {
+        await AsyncStorage.multiRemove(['@Barbeiro:token', '@Barbeiro:user']);
+        setData({} as AuthState);
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = (): AuthContextData => {
+    const context = useContext(AuthContext);
+
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+
+    return context;
+};
--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,19 +1,71 @@
-import React, { useRef } from 'react';
-import { Image, ScrollView, View, KeyboardAvoidingView, Platform } from 'react-native'
+import React, { useRef, useCallback } from 'react';
+import { Image, ScrollView, View, KeyboardAvoidingView, Platform, TextInput, Alert } from 'react-native'
 import FeatherIcon from 'react-native-vector-icons/Feather'
+import * as Yup from 'yup';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button'
 import { Form } from "@unform/mobile";
 import { FormHandles } from "@unform/core";
-
+import getValidationErrors from '../../utils/getValidationErrors';
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 import logoImg from '../../assets/logo.png'
 import { useNavigation } from '@react-navigation/native';
+import api from '../../services/api';
+
+interface SignUpFormData {
+    name: string;
+    email: string;
+    password: string;
+}
 
 const SignIn: React.FC = () => {
     const navigation = useNavigation();
+    const emailInputRef = useRef<TextInput>(null);
+    const passwordInputRef = useRef<TextInput>(null);
     const formRef = useRef<FormHandles>(null);
+
+    const handleSignUp = useCallback(
+        async (data: SignUpFormData) => {
+            try {
+                // Limpa erros do form
+                formRef.current?.setErrors({});
+
+                const schema = Yup.object().shape({
+                    name: Yup.string().required('Nome obrigatório.'),
+                    email: Yup.string()
+                        .required('E-mail obrigatório.')
+                        .email('Digite um e-mail válido.'),
+                    password: Yup.string().min(
+                        6,
+                        'A senha deve ter no mínimo 6 digitos.',
+                    ),
+                });
+                await schema.validate(data, {
+                    abortEarly: false,
+                });
+
+                Alert.alert(
+                    'Cadastro realizado com sucesso!',
+                    'Você já pode fazer login!'
+                )
+                await api.post('/users', data);
+                navigation.goBack();
+
+            } catch (err) {
+                if (err instanceof Yup.ValidationError) {
+                    const errors = getValidationErrors(err);
+                    formRef.current?.setErrors(errors);
+                    return;
+                }
+                Alert.alert(
+                    'Erro no cadastro',
+                    'Ocorreu um erro no cadastro.',
+                );
+            }
+        },
+        [navigation],
+    );
 
     return (
         <>
@@ -29,10 +81,42 @@ const SignIn: React.FC = () => {
                         <View>
                             <Title>Crie sua conta</Title>
                         </View>
-                        <Form ref={formRef} onSubmit={() => { }}>
-                            <Input name="name" icon="user" placeholder="Nome" />
-                            <Input name="email" icon="mail" placeholder="E-mail" />
-                            <Input name="password" icon="lock" placeholder="Senha" />
+                        <Form ref={formRef} onSubmit={handleSignUp}>
+                            <Input
+                                autoCapitalize="words"
+                                name="name"
+                                icon="user"
+                                placeholder="Nome"
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    emailInputRef.current?.focus()
+                                }}
+                            />
+                            <Input
+                                ref={emailInputRef}
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                keyboardType="email-address"
+                                name="email"
+                                icon="mail"
+                                placeholder="E-mail"
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    passwordInputRef.current?.focus()
+                                }}
+                            />
+                            <Input
+                                ref={passwordInputRef}
+                                secureTextEntry
+                                name="password"
+                                icon="lock"
+                                placeholder="Senha"
+                                textContentType="newPassword"
+                                returnKeyType="send"
+                                onSubmitEditing={() => {
+                                    formRef.current?.submitForm()
+                                }}
+                            />
 
                             <Button onPress={() => {
                                 formRef.current?.submitForm()
