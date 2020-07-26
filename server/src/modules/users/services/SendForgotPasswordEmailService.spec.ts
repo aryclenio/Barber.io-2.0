5c52d39b86fcbd3a779dabac6e6575a29e124593
Aryclenio Xavier Barros <aryclenio2010@gmail.com>
2020-07-26 12:49:57 -0300

    :rocket: Creating TDD for ForgotPassword

---
+++ server/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,31 @@
+import 'reflect-metadata';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider'
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider
+    );
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
