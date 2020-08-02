2de809336c717674c7d8f2ca61c1acae8af36a86
Aryclenio Xavier Barros <aryclenio2010@gmail.com>
2020-08-02 12:34:08 -0300

    :rocket: Implementing user_id in appointments

--- server/src/modules/appointments/services/CreateAppointmentService.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.ts
@@ -6,6 +6,7 @@ import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
   provider_id: string;
+  user_id: string;
   date: Date;
 }
 @injectable()
@@ -14,9 +15,9 @@ class CreateAppointmentService {
     // variavel criada automaticamente após inserção do private
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
-  ) {}
+  ) { }
 
-  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
+  public async execute({ date, provider_id, user_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
@@ -29,6 +30,7 @@ class CreateAppointmentService {
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
     });
 
